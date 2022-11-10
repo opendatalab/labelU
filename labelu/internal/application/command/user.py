54240c4b912088b3b685a1aa4a73ee2253552e0a
pengjinhu <pengjinhu@pjlab.org.cn>
2022-11-10 16:42:15 +0800

    sign up and login limit password lenght

--- labelu/internal/application/command/user.py
+++ labelu/internal/application/command/user.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, EmailStr, Field
 
 class SignupCommand(BaseModel):
     username: EmailStr
-    password: str
+    password: str = Field(min_length=6, max_length=18)
 
 
 class LoginCommand(BaseModel):
     username: EmailStr
-    password: str
+    password: str = Field(min_length=6, max_length=18)
