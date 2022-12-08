6156d7ba1683475da9ffa42f481bef79ddb16b97
pengjinhu <pengjinhu@pjlab.org.cn>
2022-12-08 15:35:14 +0800

    remove logging api response

--- labelu/internal/middleware/tracing.py
+++ labelu/internal/middleware/tracing.py
@@ -11,9 +11,10 @@ class TracingMiddleWare(BaseHTTPMiddleware):
 
         response = await call_next(request)
 
-        response_body = [chunk async for chunk in response.body_iterator]
-        response.body_iterator = iterate_in_threadpool(iter(response_body))
-
+        # uncomments for logging response body
+        # response_body = [chunk async for chunk in response.body_iterator]
+        # response.body_iterator = iterate_in_threadpool(iter(response_body))
         # logger.info(f"response_body={(b''.join(response_body)).decode()}")
+
         logger.info(f"{scope['path']} end")
         return response
