1fc9ea24c1106786e45f23c2a0c3b3c2bb732d7b
pengjinhu <pengjinhu@pjlab.org.cn>
2023-02-02 15:51:27 +0800

    remove get pre sample detail

--- labelu/internal/adapter/persistence/crud_sample.py
+++ labelu/internal/adapter/persistence/crud_sample.py
@@ -75,19 +75,6 @@ def get(db: Session, sample_id: int) -> TaskSample:
     )
 
 
-def get_pre(db: Session, task_id: int, sample_id: int) -> TaskSample:
-    return (
-        db.query(TaskSample)
-        .filter(
-            TaskSample.task_id == task_id,
-            TaskSample.id < sample_id,
-            TaskSample.deleted_at == None,
-        )
-        .order_by(TaskSample.id.desc())
-        .first()
-    )
-
-
 def get_by_ids(db: Session, sample_ids: List[int]) -> List[TaskSample]:
     return (
         db.query(TaskSample)
