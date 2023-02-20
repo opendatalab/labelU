10fbc317cfe89f1ab3ba3d915acbf87b02e3a070
wuhuilan <1521061077@qq.com>
2023-02-20 18:45:48 +0800

    style：modify the description of task in media_type

--- labelu/internal/application/command/task.py
+++ labelu/internal/application/command/task.py
@@ -25,7 +25,7 @@ class UpdateCommand(BaseModel):
         default=None, description="description: task tips", max_length=1000
     )
     media_type: Union[MediaType, None] = Field(
-        default=None, description="description: task config content"
+        default=None, description="description: task media_type content"
     )
     config: Union[str, None] = Field(
         default=None, description="description: task config content"
