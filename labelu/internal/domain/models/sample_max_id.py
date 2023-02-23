cda5bb3f3530c52be1efaad7a943034b9c32cba8
wuhuilan <1521061077@qq.com>
2023-02-23 10:45:07 +0800

    feat: add a new table to store the maximum id of task sample

---
+++ labelu/internal/domain/models/sample_max_id.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, DateTime, ForeignKey, Integer
+
+from labelu.internal.common.db import Base
+
+
+class TaskSampleMaxId(Base):
+    __tablename__ = 'sample_max_id'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    task_id = Column(Integer, ForeignKey("task.id"), index=True)
+    sample_max_id = Column(Integer, comment="The max sample id of task")
+    create_by = Column(Integer, ForeignKey("user.id"), index=True)
+    create_at = Column(DateTime, default=datetime.now, comment="The first time to create sample of a task")
+    update_by = Column(Integer, ForeignKey="user.id")
+    update_at = Column(DateTime, default=datetime.now, comment="Last time a sample was updated")
+
+    task = relationship("Task", foreign_keys=[task_id])
+    owner = relationship("User", foreign_keys=[create_by])
+    updater = relationship("User", foreign_keys=[update_by])
