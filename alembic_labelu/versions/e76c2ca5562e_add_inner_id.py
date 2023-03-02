81c0cd173c069c796b97961952a25ce32340c0c7
wuhuilan <1521061077@qq.com>
2023-03-02 15:45:42 +0800

    feat: add alembic_labelu_tools to judge inner_id and last_sample_inner_id columns exist in old tables

--- alembic_labelu/versions/e76c2ca5562e_add_inner_id.py
+++ alembic_labelu/versions/e76c2ca5562e_add_inner_id.py
@@ -5,11 +5,19 @@ Revises: 54fee6a7ecd8
 Create Date: 2023-02-28 22:29:31.595257
 
 """
+import imp
+import os
+
 from alembic import op
 from alembic import context
 import sqlalchemy as sa
 from sqlalchemy.sql import text
 
+# import alembic_labelu_tools from the absolute path
+alembic_labelu_tools = imp.load_source(
+    "alembic_labelu_tools",
+    (os.getcwd() + "/" + op.get_context().script.dir + "/alembic_labelu_tools.py"),
+)
 
 # revision identifiers, used by Alembic.
 revision = "e76c2ca5562e"
@@ -21,26 +29,35 @@ depends_on = None
 def upgrade() -> None:
     # add inner_id and last_sample_inner_id columns in the task and sample tables
     with context.begin_transaction():
-
-        op.add_column(
-            "task",
-            sa.Column(
-                "last_sample_inner_id",
-                sa.Integer(),
-                nullable=True,
-                comment="The last inner id of sample in a task",
-                server_default=text("0"),
-            ),
-        )
-        op.add_column(
-            "task_sample",
-            sa.Column(
-                "inner_id", sa.Integer(), nullable=True, comment="sample id in a task"
-            ),
-        )
-        op.create_index(
-            op.f("ix_task_sample_inner_id"), "task_sample", ["inner_id"], unique=False
-        )
+        if not alembic_labelu_tools.column_exist_in_table(
+            "task", "last_sample_inner_id"
+        ):
+            op.add_column(
+                "task",
+                sa.Column(
+                    "last_sample_inner_id",
+                    sa.Integer(),
+                    nullable=True,
+                    comment="The last inner id of sample in a task",
+                    server_default=text("0"),
+                ),
+            )
+        if not alembic_labelu_tools.column_exist_in_table("task_sample", "inner_id"):
+            op.add_column(
+                "task_sample",
+                sa.Column(
+                    "inner_id",
+                    sa.Integer(),
+                    nullable=True,
+                    comment="sample id in a task",
+                ),
+            )
+            op.create_index(
+                op.f("ix_task_sample_inner_id"),
+                "task_sample",
+                ["inner_id"],
+                unique=False,
+            )
 
         op.execute(
             "UPDATE task SET last_sample_inner_id=(SELECT sample_number FROM ( SELECT task_id, COUNT(id) AS sample_number FROM task_sample GROUP BY task_id ) task_sample_number WHERE task.id=task_sample_number.task_id )"
@@ -49,12 +66,9 @@ def upgrade() -> None:
             "UPDATE task_sample SET inner_id=(SELECT newid FROM ( SELECT id, task_id, ROW_NUMBER() OVER (PARTITION BY task_id ORDER BY created_at) AS newid FROM task_sample) tmp_sample_inner_id WHERE task_sample.id=tmp_sample_inner_id.id and task_sample.task_id=tmp_sample_inner_id.task_id)"
         )
 
-    # ### end Alembic commands ###
-
 
 def downgrade() -> None:
     # delete inner_id and last_sample_inner_id columns in the task and sample tables
     op.drop_index(op.f("ix_task_sample_inner_id"), table_name="task_sample")
     op.drop_column("task_sample", "inner_id")
     op.drop_column("task", "last_sample_inner_id")
-    # ### end Alembic commands ###
