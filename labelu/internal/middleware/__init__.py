d421ac2f8f2b682aea221219c4cf17ba2ed955da
gary-Shen <grinson50@gmail.com>
2023-11-07 12:47:47 +0800

    docs: only docs

--- labelu/internal/middleware/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from labelu.internal.common.config import settings
-from labelu.internal.middleware.tracing import TracingMiddleWare
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
-
-
-def add_middleware(app: FastAPI):
-    app.add_middleware(TracingMiddleWare)
