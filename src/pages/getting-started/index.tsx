937329ea35539ccf9d3c07dc886685094fd39a14
gary-Shen <grinson50@gmail.com>
2023-11-09 17:17:07 +0800

    chore: update i18n translation

--- src/pages/getting-started/index.tsx
+++ src/pages/getting-started/index.tsx
@@ -1,5 +1,18 @@
-import Markdown from './getting-started.mdx';
+import { useTranslation } from 'react-i18next';
+import loadable from '@loadable/component';
+
+const Markdown_zh_CN = loadable(() => import('./getting-started-zh-CN.mdx'));
+const Markdown_en_US = loadable(() => import('./getting-started-en-US.mdx'));
+
+const fileMap: Record<string, React.ElementType> = {
+  'zh-CN': Markdown_zh_CN,
+  'en-US': Markdown_en_US,
+};
 
 export default function GettingStarted() {
+  const { i18n } = useTranslation();
+
+  const Markdown = fileMap[i18n.language] || null;
+
   return <Markdown />;
 }
