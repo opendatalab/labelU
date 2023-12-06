42b0b4ed0b07a5c866229b949394e4888e602a92
gary-Shen <grinson50@gmail.com>
2023-12-06 14:31:20 +0800

    fix: update download script

--- scripts/resolve_frontend.sh
+++ scripts/resolve_frontend.sh
@@ -39,7 +39,7 @@ if [ $# -gt 1 ]; then
     echo "alpha_version: $alpha_version" >> .VERSION
   fi
 else
-  if [ "$CURRENT_BRANCH" = "main" ]; then
+  if [ "$CURRENT_BRANCH" = "main" ] || [[ "$CURRENT_BRANCH" = release/* ]]; then
     url=$release_assets_url
   else
     url=$alpha_assets_url
