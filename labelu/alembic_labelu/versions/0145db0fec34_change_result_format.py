513dc02a9794ca8d0131a8bb5b76053c5e5c9b65
gary-Shen <grinson50@gmail.com>
2024-02-06 15:37:37 +0800

    update: recover transcript

--- labelu/alembic_labelu/versions/0145db0fec34_change_result_format.py
+++ labelu/alembic_labelu/versions/0145db0fec34_change_result_format.py
@@ -37,7 +37,7 @@ class NewResult(BaseModel):
     textAttribute: Optional[str] = None
     order: Optional[int] = None
     attributes: Optional[dict] = None
-    points: Optional[list] = None
+    pointList: Optional[list] = None
     label: Optional[str] = None
     isReference: Optional[bool] = None
     disableDelete: Optional[bool] = None
@@ -58,7 +58,7 @@ class OldResult(BaseModel):
     textAttribute: Optional[str] = None
     order: Optional[int] = None
     result: Optional[dict] = None
-    points: Optional[list] = None
+    pointList: Optional[list] = None
     label: Optional[str] = None
     isReference: Optional[bool] = None
     disableDelete: Optional[bool] = None
@@ -77,7 +77,7 @@ class OldResult(BaseModel):
             sourceID=self.sourceID,
             textAttribute=self.textAttribute,
             order=self.order,
-            points=self.points,
+            pointList=self.pointList,
             label=self.label,
             isReference=self.isReference,
             disableDelete=self.disableDelete,
