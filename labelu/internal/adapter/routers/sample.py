8314609495c6e495fa5f43adea3bb74b1e5ae2b4
gary-shen <grinson50@gmail.com>
2024-04-19 14:09:29 +0800

    fix: add updated_at sort for samples

--- labelu/internal/adapter/routers/sample.py
+++ labelu/internal/adapter/routers/sample.py
@@ -65,7 +65,7 @@ async def list_by(
     pageNo: Union[int, None] = Query(default=None, ge=0),
     pageSize: Union[int, None] = 100,
     sort: Union[str, None] = Query(
-        default=None, regex="(annotated_count|state):(desc|asc)"
+        default=None, regex="(annotated_count|state|inner_id|updated_at):(desc|asc)"
     ),
     authorization: HTTPAuthorizationCredentials = Security(security),
     db: Session = Depends(db.get_db),
