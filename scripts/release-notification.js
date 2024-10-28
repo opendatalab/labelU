83f43eb9d03390686a41dc22666a058190bfb63c
gary-Shen <grinson50@gmail.com>
2024-10-28 12:47:24 +0800

    fix: test notification action

---
+++ scripts/release-notification.js
@@ -0,0 +1,63 @@
+const process = require('node:process')
+
+const { httpPost, makeLarkMessage } = require('./utils')
+
+function format(input) {
+  const rows = input.split('\n')
+  const stringRows = []
+
+  for (let i = 1; i < rows.length; i++) {
+    // 空行或者换行符忽略
+    if (rows[i].trim() === '' || rows[i].trim() === '\n') {
+      continue
+    }
+
+    let row = rows[i].replace(/#/g, '')
+    stringRows.push(row.trim())
+  }
+
+  return stringRows.join('\n')
+}
+
+async function main() {
+  const releaseNotes = process.env.RELEASE_NOTES
+  const frontendChangelog = `${process.env.CHANGELOG}`
+  const botUrl = process.env.WEBHOOK_URL
+  const releaseTag = process.env.NEXT_VERSION
+  const pypiUrl = process.env.PYPI_URL
+
+  console.log('url', botUrl)
+
+  try {
+    const payload = makeLarkMessage(
+      `LabelU@v${releaseTag}`,
+      `${format(releaseNotes)}\n\n**Frontend Changelog**\n${format(frontendChangelog)}`,
+      '点击访问',
+      pypiUrl,
+    )
+
+    // 将 release 信息通过 POST 请求发送到指定 URL
+    console.log(JSON.stringify(payload))
+    // return
+    const postResponse = await httpPost(botUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }, payload)
+
+    const responseBody = JSON.parse(postResponse)
+
+    if (responseBody.code > 0) {
+      throw new Error(`POST 请求失败: ${responseBody.msg}`)
+    }
+
+    console.log('Release 信息已成功发送')
+  }
+  catch (error) {
+    console.error('发生错误:', error)
+    process.exit(1)
+  }
+}
+
+main()
