1ae0b7f1c8e2543f7626115e728e5980bd0aef8d
gary-Shen <grinson50@gmail.com>
2024-10-29 12:02:53 +0800

    chore: delete test script

--- test.sh
+++
@@ -1,3 +0,0 @@
-BACKEND_VERSION=$(grep '^version =' pyproject.toml | sed -E 's/version = ["'\''](.*)["'\'']/\1/')
-BACKEND_VERSION=$(echo $BACKEND_VERSION | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)-alpha\.([0-9]+)/\1a\2/')
-echo "backend_version: $BACKEND_VERSION"
