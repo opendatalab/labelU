51dd50f0b91624d90610e7d79981e874bae40c11
gary-Shen <grinson50@gmail.com>
2025-03-13 22:46:28 +0800

    update: add migration script

---
+++ labelu/alembic_labelu/versions/034c7045b540_remove_task_attachment_ids.py
@@ -0,0 +1,24 @@
+"""remove_task_attachment_ids
+
+Revision ID: 034c7045b540
+Revises: 2eb983c9a254
+Create Date: 2025-03-13 22:43:30.564428
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '034c7045b540'
+down_revision = '2eb983c9a254'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
